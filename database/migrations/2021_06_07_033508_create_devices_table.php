7a26d24599c38fbd7f2af318e194a9e84dccc82f
Noel Emmanuel Roodly <emmanuel.noel@wyzdev.net>
2021-06-07 01:33:57 -0400

    changes

---
+++ database/migrations/2021_06_07_033508_create_devices_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDevicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('devices', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('fingerprint', 60)->unique();
+            $table->string('browser_info', 200)->nullable();
+            $table->string('system_info', 200)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('devices');
+    }
+}
---
+++ database/migrations/2021_06_07_033722_create_user_logs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_logs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->string('browser_fingerprint', 60);
+            $table->string('browser')->nullable();
+            $table->string('user_timezone')->nullable();
+            $table->string('user_system')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_logs');
+    }
+}
